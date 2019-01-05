0277c8a07e7a263a4fd8440600a83fa9ed9ba7e2
chan <986632543@qq.com>
2019-01-05 18:30:13 +0800

    测试异常捕获机制

--- spring/src/main/java/com/controller/Controller.java
+++ spring/src/main/java/com/controller/Controller.java
@@ -1,15 +1,25 @@
 package com.controller;
 
+import com.aspect.Method;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 @org.springframework.stereotype.Controller
+@Component
 @RequestMapping("/")
 public class Controller {
+  @Autowired
+  private Method method;
   @RequestMapping(value = "/{para}",method = RequestMethod.GET)
-  public String hello(@PathVariable("para") String para) {
+  public String hello(@PathVariable("para") String para)  {
     System.out.println(para);
+    try {
+      method.method();
+    } catch (Exception e) {
+      System.out.println("self handle");
+    }
     return "hello";
   }
 }
